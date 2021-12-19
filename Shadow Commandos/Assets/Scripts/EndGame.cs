7eaee2c63bcd23ddb7216c53f172167e8a2e1ac4
Akseli <53864034+ileska92@users.noreply.github.com>
2021-12-19 21:44:14 +0200

    Player Death and Game Ending

--- Shadow Commandos/Assets/Scripts/EndGame.cs
+++ Shadow Commandos/Assets/Scripts/EndGame.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EndGame : MonoBehaviour
 {
+    public GameObject pauseMenuUI;
+    public GameObject resume;
+    public GameObject settings;
+    public GameObject volume;
+    public GameObject slider;
+    public GameObject endText;
+    public GameObject disableGun;
+
+    private void Start()
+    {
+        disableGun = GameObject.Find("Gun");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Time.timeScale = 0;
+            pauseMenuUI.SetActive(true);
+            resume.SetActive(false);
+            settings.SetActive(false);
+            volume.SetActive(false);
+            slider.SetActive(false);
+            endText.GetComponent<TextMeshProUGUI>().enabled = true;
+            disableGun.SetActive(false);
+            gameObject.SetActive(false);
         }
     }
 }
