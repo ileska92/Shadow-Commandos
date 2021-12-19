5039b0b6c3315ed10c766c0150692f15af98865f
Akseli <53864034+ileska92@users.noreply.github.com>
2021-12-19 23:07:49 +0200

    Music and enemyCollider fix

--- Shadow Commandos/Assets/Scripts/EnemyAI/ActivateEnemy.cs
+++ Shadow Commandos/Assets/Scripts/EnemyAI/ActivateEnemy.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class ActivateEnemy : MonoBehaviour
 {
     public GameObject enemy1, enemy2, enemy3, enemy4;
+    public AudioClip zombieSound;
+    public AudioSource myAudio;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(zombieSound, this.gameObject.transform.position);
             enemy1.SetActive(true);
             enemy2.SetActive(true);
             enemy3.SetActive(true);
